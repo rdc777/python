4d620c93059e792bc0cbb5422494d6b4d0ee2271
rdc777 <109950779+rdc777@users.noreply.github.com>
2022-08-25 12:48:26 +0900

    Update 6__bs4.py
    검색값에 따른 페이지 제목 엑셀에 저장

--- 6__bs4.py
+++ 6__bs4.py
@@ -1,23 +1,73 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
+import re
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import selenium
+import pyautogui
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import datetime
 
 from openpyxl import Workbook
+browser = webdriver.Chrome(r"C:\file_python\chromedriver")
+browser.implicitly_wait(10)
 wb = Workbook()
 ws = wb.active
-ws.title = "NadoSheet"
+ws.title = "웹 크롤링"
 
-for page in range(1,100, 10):
+def makePgNum(num):
+    if num == 1:
+        return num
+    elif num == 0:
+        return num+1
+    else:
+        return num+9*(num-1)
+
+def makeUrl(search,start_pg,end_pg):
+    if start_pg == end_pg:
+        start_page = makePgNum(start_pg)
+        url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(start_page)
+        print("생성url: ",url)
+        return url
+    else:
+        urls= []
+        for i in range(start_pg,end_pg+1):
+            page = makePgNum(i)
+            url = "https://search.naver.com/search.naver?where=news&sm=tab_pge&query=" + search + "&start=" + str(page)
+            urls.append(url)
+        print("생성url: ",urls)
+        return urls
+
+search = input("검색할 키워드를 입력해주세요:")
+
+#검색 시작할 페이지 입력
+page = int(input("\n크롤링할 시작 페이지를 입력해주세요. ex)1(숫자만입력):")) # ex)1 =1페이지,2=2페이지...
+print("\n크롤링할 시작 페이지: ",page,"페이지")   
+#검색 종료할 페이지 입력
+page2 = int(input("\n크롤링할 종료 페이지를 입력해주세요. ex)1(숫자만입력):")) # ex)1 =1페이지,2=2페이지...
+print("\n크롤링할 종료 페이지: ",page2,"페이지")   
+
+# naver url 생성
+search_urls = makeUrl(search,page,page2)
+b = 0
+for page in search_urls:
     #print(page)
-    raw = requests.get(r"https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%A3%BC%EC%8B%9D&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=98&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start="+str(page))
+    raw = requests.get(page)
     html = BeautifulSoup(raw.text, "html.parser")
 
-    clips = html.select("ul.list_news > li")
+
+
+    clips = html.select("ul.list_news > li")##sp_nws99//*[@id="sp_nws99"]#sp_nws99
     # clips = html.select("a.news_tit")
     #print(clips[0].text)
 
     # c = clips[0].select_one('a.news_tit')
+    b = b+1
     a = 0
-    a = a + page
+    a = a + b
     for c in clips:
         a = a + 1
         title = c.select_one('a.news_tit')
@@ -25,41 +75,5 @@ for page in range(1,100, 10):
         ws["A" + str(a)] = title.text
     
 wb.save(r"C:\file_python\samp.xlsx")
-
-# print(c.text)
-
-
-# print(clips[0])
-# 컨테이너 소스코드는 주석처리해줍니다.
-
-# for cl in clips:
-#     title = cl.select_one("dt.title")
-#     title1 = cl.select_one("dd.chn")
-#     title2 = cl.select_one("span.hit")
-#     title3 = cl.select_one("span.like")
-#     print(title.text.strip())
-#     print(title1.text.strip())
-#     print(title2.text.strip())
-#     print(title3.text.strip())
-# #print(soup.title)
-# print(soup.title.get_text())
-# print(soup.a.get_text())
-# print(soup.a.attrs)
-# print(soup.a["href"])
-
-#sp_nws1 > div.news_wrap.api_ani_send > div > a
-# print(soup.find("a",attrs={"class":"Nbtn_upload"}))
-# print(soup.find(attrs={"class":"Nbtn_upload"}))
-
-# print(soup.find("li",attrs={"class":"rank01"}))
-# rank1 =soup.find("li",attrs={"class":"rank01"})
-# rank2 =rank1.next_sibling.next_sibling
-# rank3 =rank2.next_sibling.next_sibling
-
-# print(rank1.a)
-
-# print(rank3.a.get_text())
-# # print(rank3.next_sibling)
-# rank2 = rank3.previous_sibling.previous_sibling
-# print(rank2.next_sibling)
-# print(rank1)
+ # 브라우저 종료하기
+browser.quit()
